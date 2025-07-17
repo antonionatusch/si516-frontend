7bb503609a546bf24e6df9df886efc7151545e80
Antonio Natusch <anthony2004951@gmail.com>
2025-07-16 21:05:57 -0400

    feat: initial commit

---
+++ src/components/Sample.jsx
@@ -0,0 +1,5 @@
+export const Sample = () => {
+  return (
+    <div className="text-3xl font-bold underline">Sample from Components dir and tailwind css styling works!</div>
+  )
+}
---
+++ src/services/Sample.jsx
@@ -0,0 +1,5 @@
+export const Sample = () => {
+  return (
+    <div>Hello World!</div>
+  )
+}
